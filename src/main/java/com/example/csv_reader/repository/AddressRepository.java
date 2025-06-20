a345aa919678b94f2fbb2c2986b4cccdc052f856
Nilesh Hupare <124909226+NileshsGithub@users.noreply.github.com>
2025-06-20 12:25:49 +0530

    Delete src directory

--- src/main/java/com/example/csv_reader/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.csv_reader.repository;
-
-import com.example.csv_reader.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
